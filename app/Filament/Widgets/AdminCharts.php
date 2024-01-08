b25ad3a776d263bde0045c3e5e1a262613aa42d6
Hermogenes Junior <shampooms@gmail.com>
2024-01-08 07:04:44 -0300

    ajustes de chart no widget

--- app/Filament/Widgets/AdminCharts.php
+++ app/Filament/Widgets/AdminCharts.php
@@ -2,25 +2,40 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Eleitores;
+use Carbon\Carbon;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class AdminCharts extends ChartWidget
 {
+
+    protected static ?string $pollingInterval = '3600s';
+    protected int | string | array $columnSpan = 'full';
+    protected static ?string $maxHeight = '300px';
     protected static ?int $sort = 2;
     protected static ?string $heading = 'Chart';
 
     protected function getData(): array
     {
+
+        $data = Trend::model(Eleitores::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
         return [
             'datasets' => [
                 [
-
-                    'label' => 'Cadastro de eleitores por semana',
-
-                    'data' => [0, 1, 2, 3, 4, 5, 6]
-                ]
+                    'label' => 'Quantidade por mês',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
                 ],
-            'labels'  => ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
     }
 
@@ -28,6 +43,32 @@ class AdminCharts extends ChartWidget
     {
         return 'line';
     }
+
+    private function getEleitoresPorMes(): array
+    {
+        $now = Carbon::now();
+
+        $eleitoresPorMes = [];
+
+        $meses = collect(range(1, 12))->map(function($mes) use ($now, $eleitoresPorMes){
+
+            $count = Eleitores::whereMonth('created_at', Carbon::parse($now->month($mes)->format('Y-m')))->count();
+
+            $eleitoresPorMes[] = $count;
+
+
+            return $now->month($mes)->format('M');
+
+        })->toArray();
+
+        return [
+            'eleitoresPorMes' => $eleitoresPorMes,
+
+            'meses' => $meses
+        ];
+
+    }
+
     public static function canView(): bool
     {
         return auth()->user()->hasRole('Admin');
